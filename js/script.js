c0d1cf4e25411c7167adea6912750ce77420e0dd
Harry-1874 <harshit1084.be21@chitkarauniversity.edu.in>
2023-10-22 15:13:35 +0530

    Home section done

--- js/script.js
+++ js/script.js
@@ -33,3 +33,22 @@ window.onload = () =>{
     }
 }
 
+var swiper = new Swiper(".phone-slider", {
+    loop:true,
+    centeredSlider:true,
+    autoplay:{
+        delay: 9500,
+        disableOnInteraction : false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
