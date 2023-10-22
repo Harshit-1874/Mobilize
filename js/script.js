8d4d4121080979e16ad8a724525a297030ab386c
Harry-1874 <harshit1084.be21@chitkarauniversity.edu.in>
2023-10-23 02:28:51 +0530

    new added

--- js/script.js
+++ js/script.js
@@ -52,3 +52,31 @@ var swiper = new Swiper(".phone-slider", {
       },
     },
   });
+
+  var swiper = new Swiper(".new-slider", {
+    spaceBetween:10,
+    loop:true,
+    centeredSlider:true,
+    autoplay:{
+        delay: 9500,
+        disableOnInteraction : false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      450: {
+        slidesPerView: 2
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 4,
+      },
+    },
+  });
