d87fd538a18aa6183dc41ba88b07014e66615897
Harry-1874 <harshit1084.be21@chitkarauniversity.edu.in>
2023-10-22 12:51:22 +0530

    Login Page added

--- js/script.js
+++ js/script.js
@@ -4,6 +4,17 @@ document.querySelector('#search-btn').onclick = () =>{
     searchForm.classList.toggle('active');
 }
 
+let loginForm = document.querySelector(".login-form-container");
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+}
+
+document.querySelector('#close-login-btn').onclick = () =>{
+    loginForm.classList.remove('active');
+}
+
+
 window.onscroll = () =>{
 
     searchForm.classList.remove('active');
@@ -21,3 +32,4 @@ window.onload = () =>{
         document.querySelector('.header .header-2').classList.remove('active');
     }
 }
+
